0c49ddea6751be514711a140cd540d1cf60d1715
Dekoder-py <kyle@codingcorner.dev>
2026-01-08 14:46:21 +1300

    make it loop + add more options

--- main.go
+++ main.go
@@ -8,29 +8,35 @@ import (
 )
 
 func main() {
-	options := [2]string{"happy", "sad"}
+	options := [8]string{"happy", "sad", "angry", "mad", "tired", "excited", "annoyed", "hopeful"}
 
 	fmt.Println("Welcome to QuoteMood!")
-	fmt.Println("Mood Options:")
-	for _, opt := range options {
-		fmt.Println("- " + opt)
-	}
 
-	fmt.Println("Enter your mood:")
-	fmt.Print(">> ")
+	var running bool = true
 
-	// get mood from user input
-	reader := bufio.NewReader(os.Stdin)
-	mood, _ := reader.ReadString('\n')
-	mood = strings.TrimSpace(strings.ToLower(mood))
+	for running {
+		fmt.Println("Mood Options:")
+		for _, opt := range options {
+			fmt.Println("- " + opt)
+		}
 
-	// process mood - must be one of the options else ask again.
-	switch mood {
-	case "happy":
-		fmt.Println("Happy quote")
-	case "sad":
-		fmt.Println("Sad quote")
-	default:
-		fmt.Println("Sorry, we didn't recognise that answer. Please choose one of the options.")
+		fmt.Println("Enter your mood:")
+		fmt.Print(">> ")
+		// get mood from user input
+		reader := bufio.NewReader(os.Stdin)
+		mood, _ := reader.ReadString('\n')
+		mood = strings.TrimSpace(strings.ToLower(mood))
+
+		// process mood - must be one of the options else ask again.
+		switch mood {
+		case "happy":
+			fmt.Println("Happy quote")
+			running = false
+		case "sad":
+			fmt.Println("Sad quote")
+			running = false
+		default:
+			fmt.Println("Sorry, we didn't recognise that answer. Please choose one of the options.")
+		}
 	}
 }
